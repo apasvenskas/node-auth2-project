6527214bcfaf5bb15b259508dd3b7f538578d8f6
Audrius Pasvenskas <apasvenskas@outlook.com>
2023-11-02 15:45:33 -0700

    project complete need to past the test

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,8 +1,20 @@
 const { JWT_SECRET } = require("../secrets"); // use this secret!
 const { findBy } = require('../users/users-model')
+const jwt = require('jsonwebtoken')
 
 const restricted = (req, res, next) => {
-  next()
+  const token = req.headers.authorization
+  if(!token){
+    return next({ status: 401, message: 'Token required' })
+  }
+  jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+    if (err) {
+      next({ status: 401, message: 'Token required' })
+    } else {
+      req.decodedToken =
+      next()
+    }
+  })
   /*
     If the user does not provide a token in the Authorization header:
     status 401
@@ -21,7 +33,11 @@ const restricted = (req, res, next) => {
 }
 
 const only = role_name => (req, res, next) => {
-  next()
+    if (role_name === req.decodedToken.role_name){
+      next()
+    } else {
+      next({ status: 403, message: 'This is not for you' })
+    }
   /*
     If the user does not provide a token in the Authorization header with a role_name
     inside its payload matching the role_name passed to this function as its argument:
